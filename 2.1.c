d9d73155fff089851395c22ab20d1e749e44450d
omvamja <136708203+omvamja@users.noreply.github.com>
2023-08-08 22:18:49 +0530

    Add files via upload

---
+++ 2.1.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+void main(){
+
+int a,b;
+
+
+a = 12;
+
+b =6;
+ printf("addition of %d and %d is=%d\n", a, b, a+b);
+  printf("subtraction of %d and%d\n",a,b,a-b);
+
+
+
+printf("multiplication of %d and %d is%d\n",a,b,a*b);
+ printf("division of %d and %d is%d\n", a,b,a/b);
+
+printf("modulo of %d and %d is %d", a, b, a%b);
+
+ }
